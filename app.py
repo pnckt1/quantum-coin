9587db79d397ebaa8e7586eaf9a6ec1cf0262e2a
pnckt1 <tuty00970@gmail.com>
2026-02-18 00:13:17 +0100

    enable cors for tarot frontend

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ from qiskit import QuantumCircuit, transpile
 from qiskit_ibm_runtime import QiskitRuntimeService, Sampler
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # =========================
 # ENV VARIABLES
